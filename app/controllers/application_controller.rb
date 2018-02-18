2c0233d596c6cf764fedd95af472a3958e4bc5c1
smshetty <smshetty@ncsu.edu>
2018-02-18 15:29:17 -0500

    Test commit 2

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   def hello
-    render html: "Working on the application"
+    render html: "Working on the application.."
   end
 
 end
