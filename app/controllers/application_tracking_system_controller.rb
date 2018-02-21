efb96939fc53f1713a395c6a0fbb9aa46766fd68
awadhwa3@ncsu.edu <angad8292@gmail.com>
2018-02-21 17:19:39 -0500

    Add user using devise

--- app/controllers/application_tracking_system_controller.rb
+++ app/controllers/application_tracking_system_controller.rb
@@ -1,7 +1,15 @@
 class ApplicationTrackingSystemController < ApplicationController
   def home
+    if user_signed_in?
+      respond_to do |format|
+        format.html { redirect_to jobs_url }
+      end
+    end
   end
-
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.for(:sign_up){|u| u.permit(:name,:email,:password,:is_recruiter,:is_applicant)}
+    devise_parameter_sanitizer.for(:account_update){|u| u.permit(:name,:email,:password,:current_password,:is_recruiter,:is_applicant)}
+  end
   def help
   end
 end
