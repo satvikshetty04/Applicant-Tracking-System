cca306ff05e64780f23c81e12693375d0edf156a
smshetty <smshetty@ncsu.edu>
2018-02-23 23:51:11 -0500

    Minor access fixes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,9 +8,9 @@ class UsersController < ApplicationController
     @user = User.new
   end
   def create
-    @user = User.new(:user)
+    @user = User.new(user_params)
     respond_to do |format|
-      if user.save
+      if user.save!
         format.html { redirect_to @user.index, notice: 'User was successfully created.' }
         format.json { render :show, status: :created, location: @user }
       else
@@ -19,4 +19,21 @@ class UsersController < ApplicationController
       end
     end
   end
+
+  private
+    def sign_up_params
+      params.require(:user).permit(:fname, :lname,:phone_no,:email,:password,:is_recruiter,:is_applicant)
+    end
+
+    def account_update_params
+      params.require(:user).permit(:fname, :lname,:phone_no,:name,:email,:password,:current_password,:is_recruiter,:is_applicant)
+    end
+
+    def set_user
+
+    end
+
+    def user_params
+      params.require(:user).permit(:fname, :lname,:phone_no,:name,:email,:password,:is_recruiter,:is_applicant)
+    end
 end
