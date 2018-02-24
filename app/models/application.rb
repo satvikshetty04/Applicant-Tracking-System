ced1ddb4f697f2a0af1f711b04f99b05de35c6be
smshetty <smshetty@ncsu.edu>
2018-02-23 20:23:38 -0500

    Resume upload

--- app/models/application.rb
+++ app/models/application.rb
@@ -1,10 +1,10 @@
 class Application < ApplicationRecord
   mount_uploader :resume, ResumeUploader
   belongs_to :user
-  validates :linkedin_url, format: {
+  validates :linkedin_url, allow_blank: true, format: {
       :with => /\A((http|https):\/\/)?(www\.)?(linkedin\.com\/in\/)+[a-z0-9\-]+\z/,
       :message => "Valid LinkedIn URL required."}
-  validates :portfolio_url, format: {
+  validates :portfolio_url, allow_blank: true, format: {
       :with => /\A(http|https):\/\/(www\.)?[a-z0-9]+[\-]?[a-z0-9]+\.(com|org|edu|ai|ml|co.[a-z]{2})\z/,
       :message => "Valid URL required. Please input in the form http(s)://(website)"}
   validates :gender, presence: true
