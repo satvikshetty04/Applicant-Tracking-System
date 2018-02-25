894c70f2d2be0dee979b8eb7191090c82ad31ade
smshetty <smshetty@ncsu.edu>
2018-02-25 17:59:54 -0500

    Finally fixed user creation and deletion

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,8 +11,8 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     respond_to do |format|
-      if user.save!
-        format.html { redirect_to @user.index, notice: 'User was successfully created.' }
+      if @user.save!
+        format.html { redirect_to users_path, notice: 'User was successfully created.' }
         format.json { render :show, status: :created, location: @user }
       else
         format.html { render :new }
@@ -21,8 +21,8 @@ class UsersController < ApplicationController
     end
   end
 
-  def remove
-    @user = User.find(user.id)
+  def destroy
+    @user = User.find(params[:id])
     @user.destroy
     respond_to do |format|
       format.html { redirect_to users_url, notice: 'User was successfully deleted.' }
@@ -32,18 +32,14 @@ class UsersController < ApplicationController
 
   private
     def sign_up_params
-      params.require(:user).permit(:fname, :lname,:phone_no,:email,:password,:is_recruiter,:is_applicant)
+      params.require(:user).permit(:id, :fname, :lname,:phone_no,:email,:password,:is_recruiter,:is_applicant)
     end
 
     def account_update_params
-      params.require(:user).permit(:fname, :lname,:phone_no,:name,:email,:password,:current_password,:is_recruiter,:is_applicant)
-    end
-
-    def set_user
-
+      params.require(:user).permit(:id, :fname, :lname,:phone_no,:name,:email,:password,:current_password,:is_recruiter,:is_applicant)
     end
 
     def user_params
-      params.require(:user).permit(:fname, :lname,:phone_no,:name,:email,:password,:is_recruiter,:is_applicant)
+      params.require(:user).permit(:id, :fname, :lname,:phone_no,:name,:email,:password,:is_recruiter,:is_applicant)
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,16 @@
 Rails.application.routes.draw do
   resources :applications
-  devise_for :users, :controllers => { registrations: 'registrations' }
-  resources :users, :only => [:index,:new, :create]
+  devise_for :users, :path => 'u', :controllers => { registrations: 'registrations' }
+  resources :users, :only => [:index, :new, :create, :destroy]
   get 'application_tracking_system/home'
   get 'application_tracking_system/help'
   get 'application_tracking_system/menu'
-
+  # match '/users', to: 'users#index', via: 'get'
+  get '/users', to: 'users#new'
+  match '/users/new', to: 'users#new', via: 'get'
+  match '/users', to: 'users#create', via: 'post'
+  # match '/users/:id', to: 'users#remove', via: 'delete'
   resources :application_tracking_system
-
   resources :jobs
   resources :companies
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
